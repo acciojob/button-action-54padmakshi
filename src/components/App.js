ee01a4e3b54d48040735ff6608bd045c8b2a447b
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2023-02-13 15:09:30 +0530

    modified

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,10 @@ import './../styles/App.css';
 
 const App = (props) => {
 const [msg, setMsg] = useState("");
+const [showmsg,setShowMsg]= useState(false);
 const handleClick = () => {
   setMsg("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  setShowMsg(true);
 }
  
 
@@ -12,7 +14,8 @@ const handleClick = () => {
     <div className="App" id="main">
       // Do not alter the main div
       <button id="click" onClick={handleClick}> CLICK HERE </button>
-<p className="show" id ="para">{msg}</p>
+      <div className={`msg ${setShowMsg ? false: true}`}>
+       <p id ="para">{msg}</p></div>
     </div>
   );
 }
