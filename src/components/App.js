93d71c5e6f110aa41b3f2d90a0113dcbd3797254
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2023-02-13 12:40:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,15 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-const [useState,setstate] = useState();
 
+ const msg = document.getElementById("para").textContent=
+ "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
 
   return (
     <div className="App" id="main">
       // Do not alter the main div
       <button id="click" > CLICK HERE </button>
-      <p id ="para"> "Hello, I've learnt 
-      to use the full-stack
-       evaluation tool. This makes me so happy"</p>
+      <p id ="para"onClick={msg}> </p>
     </div>
   );
 }
